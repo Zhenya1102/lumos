4796ec06608532d3cb06c8004c776818828bc849
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2024-09-26 18:15:46 +0300

    fix styles

--- ui/Componets/Block/Block.tsx
+++ ui/Componets/Block/Block.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import React, {useState} from 'react';
+import React from 'react';
 import {Checkbox} from '@/ui/Componets/Checkbox/Checkbox';
 
 type Props = {
